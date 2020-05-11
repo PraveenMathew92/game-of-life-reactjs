d6cca1593e8953720e5704012ffee8a19ff9beee
Praveen Mathew <praveen.mathew92@outlook.com>
2020-05-12 01:37:05 +0530

    Display boxes in the grid

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Game.css';
 
-const CELL_SIZE = 200;
+const CELL_SIZE = 20;
 const WIDTH = 800;
 const HEIGHT = 600;
 
@@ -9,7 +9,7 @@ class Game extends React.Component {
     render() {
         return (
             <div>
-                <div className="Board" style={{ width: WIDTH, height: HEIGHT}} />
+                <div className="Board" style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}}  />
             </div>
         )
     }
