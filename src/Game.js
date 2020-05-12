9741bed728479b6f9d0878067b2300677bdc88ca
Praveen Mathew <praveen.mathew92@outlook.com>
2020-05-12 09:50:36 +0530

    Keep Cell info

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,25 @@ const WIDTH = 800;
 const HEIGHT = 600;
 
 class Game extends React.Component {
+    constructor() {
+        super();
+        this.rows = HEIGHT/CELL_SIZE;
+        this.columns = WIDTH/CELL_SIZE;
+        this.board = this.makeEmptyBoard();
+    }
+    state = { cells: [] }
+
+    makeEmptyBoard() {
+        let board = [];
+        for(var i = 0; i <= this.rows; i++) {
+            board[i] = [];
+            for(var j = 0; j <= this.columns; j++) {
+                board[i][j] = false;
+            }
+        }
+        return board
+    }
+
     render() {
         return (
             <div>
