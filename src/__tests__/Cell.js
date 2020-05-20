5cfbbd8affbf962f6a5013f5b01eeb98760ebb22
Praveen Mathew <praveen.mathew92@outlook.com>
2020-05-20 23:54:30 +0530

    Test style prop of Cell component

--- src/__tests__/Cell.js
+++ src/__tests__/Cell.js
@@ -7,4 +7,14 @@ describe('Cell Component', () => {
         const component = shallow(<Cell />);
         expect(component.find(".Cell")).toHaveLength(1);
     });
+
+    it('should have style prop', () => {
+        const component = shallow(<Cell x="5" y="6"/>);
+        expect(component.prop("style")).toStrictEqual({
+            "height": "19px",
+            "left": "101px",
+            "top": "121px",
+            "width": "19px"
+        });
+    });
 });
